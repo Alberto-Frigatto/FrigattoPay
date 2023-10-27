e918d76a81a5db38e71635cb5582468b667da4bc
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-10-26 22:42:20 -0300

    Alterado tipo de dataAbertura em ClientePJ e adicionados setters
    E também modificado o método data

--- src/com/entity/cliente/ClientePJ.java
+++ src/com/entity/cliente/ClientePJ.java
@@ -1,5 +1,8 @@
 package com.entity.cliente;
 
+import java.text.ParseException;
+import java.util.Date;
+
 import com.entity.Endereco;
 import com.entity.Telefone;
 
@@ -7,7 +10,7 @@ public class ClientePJ extends Cliente
 {
     private String cnpj;
     private String inscricaoEstadual;
-    private String dataAbertura;
+    private Date dataAbertura;
     private String setor;
 
     public ClientePJ(
@@ -19,13 +22,14 @@ public class ClientePJ extends Cliente
         String inscricaoEstadual,
         String dataAbertura,
         String setor
-    )
+    ) throws ParseException
     {
         super(id, nome, email, senha);
         this.cnpj = cnpj;
         this.inscricaoEstadual = inscricaoEstadual;
-        this.dataAbertura = dataAbertura;
         this.setor = setor;
+
+        this.dataAbertura = this.dateFormat.parse(dataAbertura);
     }
 
     public void data()
@@ -37,7 +41,7 @@ public class ClientePJ extends Cliente
         System.out.println("\tsenha=" + this.getSenha() + ",");
         System.out.println("\tcnpj=" + this.cnpj + ",");
         System.out.println("\tinscricaoEstadual=" + this.inscricaoEstadual + ",");
-        System.out.println("\tdataAbertura=" + this.dataAbertura + ",");
+        System.out.println("\tdataAbertura=" + this.dateFormat.format(this.dataAbertura) + ",");
         System.out.println("\ttelefones=[");
 
         for (Telefone telefone : this.getTelefones())
@@ -58,24 +62,44 @@ public class ClientePJ extends Cliente
         System.out.println(")\n");
     }
 
-    public String getcnpj()
+    public String getCnpj()
     {
-      return this.cnpj;
+        return this.cnpj;
     }
 
-    public String getinscricaoEstadual()
+    public void updateCnpj(String value)
     {
-      return this.inscricaoEstadual;
+        this.cnpj = value;
     }
 
-    public String getDataAbertura()
+    public String getInscricaoEstadual()
     {
-      return this.dataAbertura;
+        return this.inscricaoEstadual;
     }
 
-    public String getsetor()
+    public void updateInscricaoEstadual(String value)
     {
-      return this.setor;
+        this.inscricaoEstadual = value;
+    }
+
+    public Date getDataAbertura()
+    {
+        return this.dataAbertura;
+    }
+
+    public void updateDataAbertura(String value) throws ParseException
+    {
+        this.dataAbertura = this.dateFormat.parse(value);
+    }
+
+    public String getSetor()
+    {
+        return this.setor;
+    }
+
+    public void updateSetor(String value)
+    {
+        this.setor = value;
     }
 }
 
