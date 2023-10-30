6d0cd3debe1e007de854b02b10b7778a5a7b6dbf
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-10-30 13:03:23 -0300

    Criada procedure InserirTipoConta

---
+++ db/procedures/InserirTipoConta.sql
@@ -0,0 +1,13 @@
+create or replace PROCEDURE InserirTipoConta(
+    p_nm_tipo_conta IN t_fp_tipo_conta.nm_tipo%type,
+)
+IS BEGIN
+
+    INSERT INTO t_fp_tipo_conta (cd_tipo, nm_tipo)
+        VALUES (
+            seq_tipo_conta.NEXTVAL,
+            p_nm_tipo
+        );
+
+    COMMIT;
+END;
