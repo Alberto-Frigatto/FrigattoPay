f6d5af425f3cc458bade9b7b05fa436c5219f4fe
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-11-02 16:37:37 -0300

    Criadas exceções relacionadas ao Endereço

---
+++ src/com/entity/endereco/EnderecoExceptions.java
@@ -0,0 +1,62 @@
+package com.entity.endereco;
+
+public class EnderecoExceptions
+{
+    public static abstract class EnderecoException extends Exception
+    {
+        public EnderecoException(String message)
+        {
+            super(message);
+        }
+    }
+
+    public static class CepInvalidoException extends EnderecoException
+    {
+        static final String MESSAGE = "CEP inválido";
+
+        public CepInvalidoException()
+        {
+            super(MESSAGE);
+        }
+    }
+
+    public static class LogradouroInvalidoException extends EnderecoException
+    {
+        static final String MESSAGE = "Logradouro inválido";
+
+        public LogradouroInvalidoException()
+        {
+            super(MESSAGE);
+        }
+    }
+
+    public static class NumeroInvalidoException extends EnderecoException
+    {
+        static final String MESSAGE = "Número inválido";
+
+        public NumeroInvalidoException()
+        {
+            super(MESSAGE);
+        }
+    }
+
+    public static class ComplementoInvalidoException extends EnderecoException
+    {
+        static final String MESSAGE = "Complemento inválido";
+
+        public ComplementoInvalidoException()
+        {
+            super(MESSAGE);
+        }
+    }
+
+    public static class MunicipioInvalidoException extends EnderecoException
+    {
+        static final String MESSAGE = "Município inválido";
+
+        public MunicipioInvalidoException()
+        {
+            super(MESSAGE);
+        }
+    }
+}
