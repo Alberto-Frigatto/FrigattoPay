1a3cf56cb3da3b9a2b449525f2d0976c65378472
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-11-02 16:33:06 -0300

    Criadas exceções relacionadas aos clientes
    ClienteExceptions.java
    ClientePFExceptions.java
    ClientePJExceptions.java

---
+++ src/com/entity/cliente/exceptions/ClienteExceptions.java
@@ -0,0 +1,42 @@
+package com.entity.cliente.exceptions;
+
+public class ClienteExceptions
+{
+    public static abstract class ClienteException extends Exception
+    {
+        public ClienteException(String message)
+        {
+            super(message);
+        }
+    }
+
+    public static class NomeInvalidoException extends ClienteException
+    {
+        static final String MESSAGE = "Nome inválido";
+
+        public NomeInvalidoException()
+        {
+            super(MESSAGE);
+        }
+    }
+
+    public static class EmailInvalidoException extends ClienteException
+    {
+        static final String MESSAGE = "Email inválido";
+
+        public EmailInvalidoException()
+        {
+            super(MESSAGE);
+        }
+    }
+
+    public static class SenhaInvalidaException extends ClienteException
+    {
+        static final String MESSAGE = "Senha inválida";
+
+        public SenhaInvalidaException()
+        {
+            super(MESSAGE);
+        }
+    }
+}
