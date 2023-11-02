4747f7f84b7d6df86cded993c17a25c38654058f
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-11-02 16:34:07 -0300

    Adicionadas validações na classe ClientePF

--- src/com/entity/cliente/ClientePF.java
+++ src/com/entity/cliente/ClientePF.java
@@ -1,10 +1,13 @@
 package com.entity.cliente;
 
 import java.text.ParseException;
+import java.util.Calendar;
 import java.util.Date;
 
-import com.entity.Endereco;
-import com.entity.Telefone;
+import com.entity.cliente.exceptions.ClienteExceptions.ClienteException;
+import com.entity.cliente.exceptions.ClientePFExceptions.*;
+import com.entity.endereco.Endereco;
+import com.entity.telefone.Telefone;
 
 public class ClientePF extends Cliente
 {
@@ -20,13 +23,15 @@ public class ClientePF extends Cliente
         String cpf,
         String rg,
         String dataNascimento
-    ) throws ParseException
+    ) throws ClienteException, ParseException
     {
         super(id, nome, email, senha);
         this.cpf = cpf;
         this.rg = rg;
 
-        this.dataNascimento = this.dateFormat.parse(dataNascimento);
+        this.validarCpf();
+        this.validarRg();
+        this.definirDataNascimentoSeValida(dataNascimento);
     }
 
     public ClientePF(
@@ -36,13 +41,103 @@ public class ClientePF extends Cliente
         String cpf,
         String rg,
         String dataNascimento
-    ) throws ParseException
+    ) throws ClienteException, ParseException
     {
         super(nome, email, senha);
         this.cpf = cpf;
         this.rg = rg;
 
-        this.dataNascimento = this.dateFormat.parse(dataNascimento);
+        this.validarCpf();
+        this.validarRg();
+        this.definirDataNascimentoSeValida(dataNascimento);
+    }
+
+    @Override
+    protected boolean nomeEValido()
+    {
+        return this.getNome() instanceof String &&
+               !this.getNome().isEmpty() &&
+               this.getNome().length() <= 50 &&
+               this.getNome().matches("^[^0-9]*$");
+    }
+
+    private void validarCpf() throws CpfInvalidoException
+    {
+        if (!this.cpfEValido())
+            throw new CpfInvalidoException();
+    }
+
+    private boolean cpfEValido()
+    {
+        if (this.cpf.matches("(\\d)\\1{10}") || this.cpf.strip().length() != 11)
+            return false;
+
+        int[] cpfDigits = new int[11];
+
+        for (int i = 0; i < 11; i++)
+            cpfDigits[i] = Character.getNumericValue(this.cpf.charAt(i));
+
+        for (int t = 9; t < 11; t++)
+        {
+            int d = 0;
+            for (int c = 0; c < t; c++)
+                d += cpfDigits[c] * ((t + 1) - c);
+
+            d = ((10 * d) % 11) % 10;
+
+            if (cpfDigits[t] != d)
+                return false;
+        }
+
+        return true;
+    }
+
+    private void validarRg() throws RgInvalidoException
+    {
+        if (!this.rgEValido())
+            throw new RgInvalidoException();
+    }
+
+    private boolean rgEValido()
+    {
+        int rgSize = 9;
+
+        if (this.rg.isEmpty() || this.rg.length() != rgSize)
+            return false;
+
+        for (char c : this.rg.toCharArray())
+            if (!Character.isDigit(c))
+                return false;
+
+        return true;
+    }
+
+    private void definirDataNascimentoSeValida(String dataNascimento) throws DataNascimentoInvalidaException
+    {
+        try
+        {
+            this.dataNascimento = this.dateFormat.parse(dataNascimento);
+        }
+        catch (ParseException e)
+        {
+            throw new DataNascimentoInvalidaException();
+        }
+
+        if (!this.dataNascimentoEValida(this.dataNascimento))
+            throw new DataNascimentoInvalidaException();
+    }
+
+    private boolean dataNascimentoEValida(Date dataNascimento)
+    {
+        Calendar calendar = Calendar.getInstance();
+
+        int anosMaioridade = 18;
+
+        calendar.add(Calendar.YEAR, -anosMaioridade);
+
+        Date dataMaioridade = calendar.getTime();
+
+        return dataNascimento.before(dataMaioridade);
     }
 
     public void data()
@@ -80,9 +175,12 @@ public class ClientePF extends Cliente
         return this.cpf;
     }
 
-    public void updateCpf(String value)
+    public void updateCpf(String value) throws CpfInvalidoException
     {
         this.cpf = value;
+
+        if (!this.cpfEValido())
+            throw new CpfInvalidoException();
     }
 
     public String getRg()
@@ -90,9 +188,12 @@ public class ClientePF extends Cliente
         return this.rg;
     }
 
-    public void updateRg(String value)
+    public void updateRg(String value) throws RgInvalidoException
     {
         this.rg = value;
+
+        if (!this.rgEValido())
+            throw new RgInvalidoException();
     }
 
     public Date getDataNascimento()
@@ -100,8 +201,8 @@ public class ClientePF extends Cliente
         return this.dataNascimento;
     }
 
-    public void updateDataNascimento(String value) throws ParseException
+    public void updateDataNascimento(String value) throws ParseException, DataNascimentoInvalidaException
     {
-        this.dataNascimento = this.dateFormat.parse(value);
+        this.definirDataNascimentoSeValida(value);
     }
 }
