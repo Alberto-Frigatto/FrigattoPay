dd66f72f75ae69942ac1328f6080561c82d3b411
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-11-04 20:46:49 -0300

    Criadas exceções relacionadas ao Cartao

---
+++ src/com/entity/conta/exceptions/CartaoExceptions.java
@@ -0,0 +1,42 @@
+package com.entity.conta.exceptions;
+
+public class CartaoExceptions
+{
+    public static abstract class CartaoException extends Exception
+    {
+        public CartaoException(String message)
+        {
+            super(message);
+        }
+    }
+
+    public static class NumeroInvalidoException extends CartaoException
+    {
+        static final String MESSAGE = "Número inválido";
+
+        public NumeroInvalidoException()
+        {
+            super(MESSAGE);
+        }
+    }
+
+    public static class DataValidadeInvalidaException extends CartaoException
+    {
+        static final String MESSAGE = "Validade inválida";
+
+        public DataValidadeInvalidaException()
+        {
+            super(MESSAGE);
+        }
+    }
+
+    public static class CvvInvalidoException extends CartaoException
+    {
+        static final String MESSAGE = "Cvv inválido";
+
+        public CvvInvalidoException()
+        {
+            super(MESSAGE);
+        }
+    }
+}
