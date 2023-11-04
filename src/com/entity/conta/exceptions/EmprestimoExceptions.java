e60900ebed1ced5fbb1b6a6e5d4ec69d3077c602
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-11-04 20:46:58 -0300

    Criadas exceções relacionadas ao Emprestimo

---
+++ src/com/entity/conta/exceptions/EmprestimoExceptions.java
@@ -0,0 +1,72 @@
+package com.entity.conta.exceptions;
+
+public class EmprestimoExceptions
+{
+    public static abstract class EmprestimoException extends Exception
+    {
+        public EmprestimoException(String message)
+        {
+            super(message);
+        }
+    }
+
+    public static class ValorInvalidoException extends EmprestimoException
+    {
+        static final String MESSAGE = "Valor inválido";
+
+        public ValorInvalidoException()
+        {
+            super(MESSAGE);
+        }
+    }
+
+    public static class ValorJurosInvalidoException extends EmprestimoException
+    {
+        static final String MESSAGE = "Valor de juros inválido";
+
+        public ValorJurosInvalidoException()
+        {
+            super(MESSAGE);
+        }
+    }
+    
+    public static class ValorParcelaInvalidoException extends EmprestimoException
+    {
+        static final String MESSAGE = "Valor de parcela inválido";
+        
+        public ValorParcelaInvalidoException()
+        {
+            super(MESSAGE);
+        }
+    }
+
+    public static class DataPrazoInvalidaException extends EmprestimoException
+    {
+        static final String MESSAGE = "Data prazo inválida";
+
+        public DataPrazoInvalidaException()
+        {
+            super(MESSAGE);
+        }
+    }
+
+    public static class DataSolicitacaoInvalidaException extends EmprestimoException
+    {
+        static final String MESSAGE = "Data de solicitação inválida";
+
+        public DataSolicitacaoInvalidaException()
+        {
+            super(MESSAGE);
+        }
+    }
+
+    public static class DiaVencimentoParcelaInvalidoException extends EmprestimoException
+    {
+        static final String MESSAGE = "Dia de vencimento da parcela inválido";
+
+        public DiaVencimentoParcelaInvalidoException()
+        {
+            super(MESSAGE);
+        }
+    }
+}
