1621519ff33f50da0ec3fae61a01b06bfea10e99
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-11-08 22:25:09 -0300

    Criada HomeServlet

---
+++ src/com/controller/HomeServlet.java
@@ -0,0 +1,24 @@
+package com.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/user/home")
+public class HomeServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		request.getRequestDispatcher("/home.jsp").forward(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		this.doPost(request, response);
+	}
+}
