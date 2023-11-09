9730c2254da851ad338c451fd371a54b5b578dca
Gustavo Lopes <guhrlopes999@gmail.com>
2023-11-08 23:12:33 -0300

    Criada Servlet ClientePjServlet

---
+++ src/com/controller/ClientePJServlet.java
@@ -0,0 +1,63 @@
+package com.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.model.dao.DAOClientePJ;
+import com.model.entity.cliente.ClientePJ;
+
+@WebServlet({"/clientePJ", "/user/clientePJ"})
+public class ClientePJServlet extends HttpServlet 
+{
+	private static final long serivalVersionUID = 1L;
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		SimpleDateFormat dateFormatInter = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Connection conn = (Connection) request.getAttribute("conn");
+		
+		try
+		{
+			DAOClientePJ daoClientePJ = new DAOClientePJ(conn);
+			
+			String nome = request.getParameter("nome");
+			String email = request.getParameter("email");
+			String cnpj = request.getParameter("cnpj");
+			String inscricaoEstadual = request.getParameter("inscricaoEstadual");
+			String setor = request.getParameter("setor");
+			
+			String rawDataAbertura = request.getParameter("dataAbertura");
+			Date dataAbertura = rawDataAbertura != "" ? dateFormatInter.parse(rawDataAbertura) : null;
+			String senha = request.getParameter("senha");
+			
+			ClientePJ cliente = new ClientePJ(
+					nome,
+					email,
+					senha,
+					cnpj,
+					inscricaoEstadual,
+					dataAbertura,
+					setor
+			);
+			
+			daoClientePJ.insert(cliente);
+			
+			request.getRequestDispatcher("index.jsp").forward(request, response);		
+		}
+		catch (Exception e)
+		{
+			request.setAttribute("error", e.getMessage());
+			e.printStackTrace();
+			request.getRequestDispatcher("register_cliente_pj.jsp").forward(request, response);
+		}
+	}
+}
