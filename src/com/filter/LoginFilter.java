e86b94cf9aa733d7689a62b1af8e9fb23dae899f
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-11-08 22:23:52 -0300

    Criado Filter LoginFilter
    Usado para bloquear o acesso páginas quando o usuário não está logado

---
+++ src/com/filter/LoginFilter.java
@@ -0,0 +1,37 @@
+package com.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/user/*")
+public class LoginFilter extends HttpFilter implements Filter
+{
+	private static final long serialVersionUID = 1L;
+
+	public void init(FilterConfig fConfig) throws ServletException {}
+	
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
+	{
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        HttpSession session = httpRequest.getSession();
+        
+        if (session.getAttribute("clienteLogado") == null)
+        	httpResponse.sendRedirect("/Fintech/index.jsp");
+        else
+        	chain.doFilter(request, response);
+	}
+
+	public void destroy() {}
+}
