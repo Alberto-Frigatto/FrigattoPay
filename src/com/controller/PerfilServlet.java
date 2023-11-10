e533fc0ba5cb49ce9ff9b9f54bbbc6df0cd01cc9
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-11-10 19:51:57 -0300

    Criada PerfilServlet

---
+++ src/com/controller/PerfilServlet.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/user/perfil")
+public class PerfilServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		request.getRequestDispatcher("/perfil.jsp").forward(request, response);
+	}
+}
