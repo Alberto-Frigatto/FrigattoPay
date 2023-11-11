e6d1e213e834a8029d3fc4093649734f81415f64
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-11-10 23:53:57 -0300

    Criada LoadPageAlterarClientePFServlet

---
+++ src/com/controller/LoadPageAlterarClientePFServlet.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/user/alterar/pf")
+public class LoadPageAlterarClientePFServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		request.getRequestDispatcher("/cliente_pf_form.jsp").forward(request, response);
+	}
+}
