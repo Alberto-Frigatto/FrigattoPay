f02cdd327a70f7aa868b2a59149a1ed59ffde753
Gustavo Lopes <guhrlopes999@gmail.com>
2023-11-11 19:25:42 -0300

    Criada LoadPageAlterarClientePJServlet

---
+++ src/com/controller/LoadPageAlterarClientePJServlet.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/user/alterar/pj")
+public class LoadPageAlterarClientePJServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		request.getRequestDispatcher("/cliente_pj_form.jsp").forward(request, response);
+	}
+}
