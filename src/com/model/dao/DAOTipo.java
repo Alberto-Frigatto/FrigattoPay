dd697d4ddb6fd54fee6957f778199ed74bf4564b
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-11-12 16:57:27 -0300

    Criado método getById em DAOTipo

--- src/com/model/dao/DAOTipo.java
+++ src/com/model/dao/DAOTipo.java
@@ -45,4 +45,30 @@ public class DAOTipo extends DAO
 
         return stmt.executeQuery(query);
     }
+	
+	public Tipo getById(String tabela, int id) throws SQLException
+    {        
+        ResultSet result = this.getTipo(tabela, id);
+
+        if (!result.next())
+        	throw new SQLException("O Tipo " + id + " não existe");
+        
+        Tipo tipo = new Tipo(
+            result.getInt("cd_tipo"),
+            result.getString("nm_tipo")
+        );
+
+        return tipo;
+    }
+
+	private ResultSet getTipo(String tabela, int id) throws SQLException
+    {		
+        String query = "SELECT cd_tipo, nm_tipo FROM " + tabela + " WHERE cd_tipo = ? ORDER BY cd_tipo";
+
+        PreparedStatement pstmt = this.conn.prepareStatement(query);
+
+        pstmt.setInt(1, id);
+
+        return pstmt.executeQuery();
+    }
 }
