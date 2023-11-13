ac2303a2341d84b8863386f93f02811a6e7e3e3c
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-11-13 19:20:19 -0300

    Criado método doPut e doDelete em ReceitaServlet

--- src/com/controller/ReceitaServlet.java
+++ src/com/controller/ReceitaServlet.java
@@ -15,21 +15,24 @@ import com.model.dao.DAOReceita;
 import com.model.entity.cliente.Cliente;
 import com.model.entity.receita.Receita;
 
-@WebServlet("/user/receita/*")
+@WebServlet("/user/receita")
 public class ReceitaServlet extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
+	{		
+		if ("delete".equals(request.getParameter("method")))
+			this.doDelete(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
-		String pathInfo = request.getPathInfo();
-		
-		if (pathInfo != null)
+	{		
+		if ("put".equals(request.getParameter("method")))
+		{
 			this.doPut(request, response);
+			return;
+		}
 		
 		Connection conn = (Connection) request.getAttribute("conn");
 		
@@ -43,7 +46,8 @@ public class ReceitaServlet extends HttpServlet
 			int idCliente = clienteLogado.getId();
 			
 			int idTipoReceita = Integer.parseInt(request.getParameter("idTipo"));
-			Double valor = Double.parseDouble(request.getParameter("valor"));
+			String rawValor = request.getParameter("valor");
+			Double valor = rawValor.isEmpty() ? 0 : Double.parseDouble(rawValor);
 			
 			String rawDataReceita = request.getParameter("dataReceita");
 			Date dataReceita = rawDataReceita != "" ? dateFormatInter.parse(rawDataReceita) : null;
@@ -68,9 +72,59 @@ public class ReceitaServlet extends HttpServlet
 
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
+		SimpleDateFormat dateFormatInter = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Connection conn = (Connection) request.getAttribute("conn");
+		
+		int idReceita = Integer.parseInt((String) request.getParameter("idReceita"));
+		
+		try
+		{
+			DAOReceita daoReceita = new DAOReceita(conn);
+			
+			int idTipoReceita = Integer.parseInt(request.getParameter("idTipo"));
+			String rawValor = request.getParameter("valor");
+			Double valor = rawValor.isEmpty() ? 0 : Double.parseDouble(rawValor);
+			
+			String rawDataReceita = request.getParameter("dataReceita");
+			Date dataReceita = rawDataReceita != "" ? dateFormatInter.parse(rawDataReceita) : null;
+			
+			
+			Receita receita = daoReceita.getById(idReceita);
+			
+			receita.updateDataReceita(dataReceita);
+			receita.updateIdTipoReceita(idTipoReceita);
+			receita.updateValor(valor);
+			
+			daoReceita.update(receita);
+
+			response.sendRedirect(request.getContextPath() + "/user/receitas");
+		}
+		catch (Exception e)
+		{
+			request.setAttribute("error", e.getMessage());
+			e.printStackTrace();
+			request.getRequestDispatcher("/user/receita/alterar?id=" + idReceita).forward(request, response);
+		}
 	}
 
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
+		Connection conn = (Connection) request.getAttribute("conn");
+		
+		try
+		{
+			DAOReceita daoReceita = new DAOReceita(conn);
+			
+			int idReceita = Integer.parseInt((String) request.getParameter("idReceita"));
+			
+			daoReceita.delete(idReceita);
+			
+			response.sendRedirect(request.getContextPath() + "/user/receitas");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
