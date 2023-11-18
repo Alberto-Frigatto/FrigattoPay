1d80f84a8f46ea2b94c30f5af6f7a21223c98c99
Gustavo Lopes <guhrlopes999@gmail.com>
2023-11-18 20:47:08 -0300

    Alterada procedure AtualizarConta

--- db/procedures/AtualizarConta.sql
+++ db/procedures/AtualizarConta.sql
@@ -1,10 +1,19 @@
 create or replace PROCEDURE AtualizarConta(
     p_cd_conta IN t_fp_conta.cd_conta%type,
-    p_vl_saldo IN t_fp_conta.vl_saldo%type
+    p_vl_saldo IN t_fp_conta.vl_saldo%type,
+    p_nr_conta IN t_fp_conta.nr_conta%type,
+    p_nr_agencia IN t_fp_conta.nr_agencia%type,
+    p_cd_tipo IN t_fp_conta.t_fp_tipo_conta_cd_tipo%type,
+    p_cd_banco IN t_fp_conta.t_fp_banco_cd_banco%type
 )
 IS BEGIN
     UPDATE t_fp_conta
-        SET vl_saldo = p_vl_saldo
+        SET vl_saldo = p_vl_saldo,
+            nr_conta = p_nr_conta,
+            nr_agencia = p_nr_agencia,
+            t_fp_tipo_conta_cd_tipo = p_cd_tipo,
+            t_fp_banco_cd_banco = p_cd_banco
+
             WHERE cd_conta = p_cd_conta;
 
     COMMIT;
